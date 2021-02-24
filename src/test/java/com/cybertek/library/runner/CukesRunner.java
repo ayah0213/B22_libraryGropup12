f8e2dc0fc7329a73fa8007127d61d12103580f67
Ayah N <ayah.noor0213@gmail.com>
2021-02-24 16:06:39 -0500

    first commit

--- src/test/java/com/cybertek/library/runner/CukesRunner.java
+++ src/test/java/com/cybertek/library/runner/CukesRunner.java
@@ -19,8 +19,8 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "com/cybertek/library/step_definitions" ,
-        dryRun = false,
-        tags = "@t5"
+        dryRun = true,
+        tags = ""
 )
 
 public class CukesRunner {
